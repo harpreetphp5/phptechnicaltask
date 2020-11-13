b96cd1099df5183247ca3c404066df6b9728950f
harpreetphp5 <50646371+harpreetphp5@users.noreply.github.com>
2020-11-13 12:11:42 +1100

    Added Comments

--- challenge.php
+++ challenge.php
@@ -3,16 +3,31 @@ declare(strict_types=1);
 final class Challenge
 {
 
-
+// draw function is to get random number of cards, e.g. if limit = 3 then you will get 3 rendom cards, 
+//where cards is an array of range from 0, n but in our case we will be using 0 to 51 cards
     public function draw($cards,$limit){
         $counter = 1;
         foreach($cards as $card){
+            //initialise an array to determine suit of the card I am assuming initially the cards will be like below
+            // 0-12 clubs, 13-25 diamonds, 26-38 hearts, 39-51 spades 
             $get_suit = array(0=>"c",1=>"d",2=>"h",3=>"s");
+            //initialise an array to determine special character cards I am assuming initially the cards will be like below
+            // 0 means Ace, 10 means Jack, 11 means Queen, and 12 means King 
             $get_alphabet = array(0=>"A",10=>"J",11=>"Q",12=>"K");
+            //getting the card's by using mathematical formula, e.g. 14 should be 2 of diamond
+            // 14/13 = 1 & 1 is diamond
             $suit = $get_suit[intval($card/13)];
+            //checking whether the card is special character card or not, 
+            // e.g. 25 should be King of diamond, 25 - 13*1 = 12 and 12 stands for King
             $BigCard = $get_alphabet[$card - 13*(intval($card/13))];
+            //adding random card's suits in a separate array to write less code in the next function.
             $suits[] = $suit;
+            //adding random card value in separate array to write less code in the next function
+            //important: $card+1 is to adjust the 0 in the range
+            //e.g. 14 number card will become 15 it will help us in next function 
+            //because in the next function we are just checking the range so it's easier to keep the numbers in numerics
             $allCards[] = $card+1;
+            //it will combine card and suits together, looks good!!
             $cardsWithSuits[] = ( $BigCard !="" ? $BigCard.$suit : ($card - 13*(intval($card/13))+1).$suit );
             if($counter==$limit){
                 return array("allCards" => $allCards,"suits" => $suits,"cardsWithSuits" => $cardsWithSuits);
